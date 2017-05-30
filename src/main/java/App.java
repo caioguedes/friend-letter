cf5118bf4d547c65fd48f9d3fc22ef240b03c89e
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-05-30 14:14:30 -0300

    Adding layout template and refactoring App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,9 @@
+import java.util.Map;
 import java.util.HashMap;
+
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
 
 public class App {
@@ -8,17 +11,22 @@ public class App {
   public static void main(String[] args) {
 
     staticFileLocation("/public");
+    String layout = "templates/layout.vm";
 
     get("/", (request, response) -> {
-      return render(new HashMap(), "templates/hello.vm");
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/hello.vm");
+      return render(model, layout);
     });
 
     get("/favorite_photos", (request, response) -> {
-      return render(new HashMap(), "templates/favorite_photos.vm");
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/favorite_photos.vm");
+      return render(model, layout);
     });
   }
 
-  public static String render(HashMap model, String templatePath) {
+  public static String render(Map model, String templatePath) {
     return new VelocityTemplateEngine().render(
       new ModelAndView(model, templatePath)
     );
